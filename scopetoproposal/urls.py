f5ca001d051581e6a0ead3db05c63f9cdd66bc8a
ohsprac <vanessa@ohspract.co.za>
2024-07-17 15:44:52 +0200

    Changed 'home' Path in scopetoproposal/urls.py

--- scopetoproposal/urls.py
+++ scopetoproposal/urls.py
@@ -8,5 +8,5 @@ from scopetoproposal.views import FileUploadView
 urlpatterns = [
     path('test/', views.test),
     path('upload/', FileUploadView.as_view(), name='file-upload'),
-    path('upload-page/', views.upload_page, name='upload-page'),
+    path('', views.upload_page, name='upload-page'),
 ]
