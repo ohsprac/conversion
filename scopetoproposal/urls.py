55cb54d69e7166f4582ade1b28594bcc018e39d9
ohsprac <vanessa@ohspract.co.za>
2024-07-18 11:45:31 +0200

    Fixes? 02

--- scopetoproposal/urls.py
+++ scopetoproposal/urls.py
@@ -8,6 +8,6 @@ from scopetoproposal.views import FileUploadView
 urlpatterns = [
     path('test/', views.test),
     path('upload/', FileUploadView.as_view(), name='file-upload'),
-    # path('', views.upload_page, name='upload-page'),
-    path('', views.maintenance, name='upload-page'),
+    path('', views.upload_page, name='upload-page'),
+    # path('', views.maintenance, name='upload-page'),
 ]
